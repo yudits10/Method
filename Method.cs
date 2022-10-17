fbd13d6e856a3d148d5c8bcea5c238d736e79b59
yudits10 <113966685+yudits10@users.noreply.github.com>
2022-10-18 04:11:44 +0700

    Add files via upload

---
+++ Method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue;
+            largerValue = Math.Max(firstValue, secondValue);
+            Console.WriteLine(largerValue);
+        }
+    }
+}
